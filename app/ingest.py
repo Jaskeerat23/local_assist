876080f254753967e99f6e9b3fee2e2ca9a29f4b
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-09-08 23:33:01 +0530

    some updates in script

--- app/ingest.py
+++ app/ingest.py
@@ -1,9 +1,18 @@
+import json
 from data_ingestion import process_dir, store
 
 def ingest_data(dir: str):
     
     # Step. 1 load docs, chunk them, give them some id
     chunk_ids, chunks = process_dir.load_and_chunk(dir)
+    
+    with open("D:/local_assist/evaluation/chunks.jsonl", "w", encoding="utf-8") as f:
+        for chunk_id, chunk in zip(chunk_ids, chunks):
+            record = {
+                "chunk_id": chunk_id,
+                "chunk_content": chunk.page_content
+            }
+            f.write(json.dumps(record, ensure_ascii=False) + "\n")
     
     # Step. 2 to convert these chunks into embeddings and then storing them to vector store
     vectorstore = store.VectorStore(persistent_dir = './my_chromadb', collection_name = 'codebase_docs')
